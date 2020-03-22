1595b82ee51851ee4efe97a09addafc6d8748208
mt.hodaka <mt.hodaka7@gmail.com>
2020-03-22 19:27:35 +0900

    Shields.ioカスタムEndpointとして公開するメソッド実装

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
 import { Lookup } from './apiClient/itunes.apple.com/lookup';
+import { ShieldsIo } from './shieldsIo';
 
 function main(): void {
-    const request = new Lookup.Request({ id: "1347707000", country: "jp" });
+    const response = itunesAppleComLookup({ id: "1347707000", country: "jp" });
+    Logger.log(ShieldsIo.Convert.toJson(response));
+}
+
+function itunesAppleComLookup({ id, country }: { id: string; country?: string; }): ShieldsIo.Response {
+    const request = new Lookup.Request({ id: id, country: country });
     const response = request.send();
-    Logger.log(response);
+    const lastReleaseDate = new Date(response.results[0].currentVersionReleaseDate);
+
+    return {
+        schemaVersion: 1,
+        label: "last release",
+        message: lastReleaseDate.toLocaleDateString(),
+        color: "orange"
+    };
 }
