5c3755a75d9a6add01013523608a365bf2e61966
mt.hodaka <mt.hodaka7@gmail.com>
2020-04-05 03:46:56 +0900

    impl doGet

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,23 @@
-import { Lookup } from './apiClient/itunes.apple.com/lookup';
+import { Usecase } from './usecase/interface/Usecase';
+import { ItunesAppleComUsecase } from './usecase/ItunesAppleComUsecase';
 import { ShieldsIo } from './shieldsIo';
 
-function main(): void {
-    const response = itunesAppleComLookup({ id: "1347707000", country: "jp" });
-    Logger.log(ShieldsIo.Convert.toJson(response));
+function doGet(e: Event): GoogleAppsScript.Content.TextOutput {
+    let response: ShieldsIo.Response
+
+    switch (e.parameter.kind) {
+        case 'ItunesAppleComUsecase.lookup':
+            response = ItunesAppleComUsecase.lookup(e.parameter);
+    }
+
+    const out = ContentService.createTextOutput();
+    out.setMimeType(ContentService.MimeType.JSON);
+    out.setContent(JSON.stringify(response));
+    return out;
 }
 
-function itunesAppleComLookup({ id, country }: { id: string; country?: string; }): ShieldsIo.Response {
-    const request = new Lookup.Request({ id: id, country: country });
-    const response = request.send();
-    const lastReleaseDate = new Date(response.results[0].currentVersionReleaseDate);
-
-    return {
-        schemaVersion: 1,
-        label: "last release",
-        message: lastReleaseDate.toLocaleDateString(),
-        color: "orange"
-    };
+interface Event {
+    queryString: string;
+    parameter: Usecase.Parameter;
+    contentLength: number;
 }
